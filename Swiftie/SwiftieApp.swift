a6251967b707e48a59961f21561ba590555fc129
jldavid <jldavid.mail@gmail.com>
2024-02-08 00:56:12 -0500

    Added files

---
+++ Swiftie/SwiftieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftieApp.swift
+//  Swiftie
+//
+//  Created by JL David on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
